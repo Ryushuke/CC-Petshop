7e8a91bd9a6d8b530a7a280c23bd23f93e8d0c99
Ryushuke <ryushuke@hotmail.com>
2025-05-30 13:48:23 -0300

    update: Adicionado o cabeçalho da pagina.

--- index.php
+++ index.php
@@ -5,8 +5,14 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title>Jorgina Petshop</title>
 	<link rel="icon" href="images/favicon.svg" type="image/svg+xml" sizes="any" />
+	<link rel="stylesheet" href="css/style.css">
+	<link rel="preconnect" href="https://fonts.googleapis.com">
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+	<link href="https://fonts.googleapis.com/css2?family=Pixelify+Sans:wght@400..700&family=Playpen+Sans+Hebrew:wght@100..800&display=swap" rel="stylesheet">
 </head>
 <body>
-	
+<?php
+include_once "view/header.php";
+?>
 </body>
 </html>
---
+++ view/header.php
@@ -0,0 +1,7 @@
+<div class="header">
+	<a href="index.php"><img class="logo" src="images/favicon.svg" width="100" height="100" /></a>
+	<span>
+		<h1>Jorgina Petshop</h1>
+		<p>Onde seu pet é tratado como rei!</p>
+	</span>
+</div>
