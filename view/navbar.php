f9be1ffd33591cf88fc95dfbc251196e8b4a80e4
ryushuke.dev@gmail.com <ryushuke.dev@gmail.com>
2025-06-02 16:32:26 -0300

    update: Barra de navegação agora contempla paginas extras, onde não é possivel navegar diretamente pela barra.

--- view/navbar.php
+++ view/navbar.php
@@ -1,7 +1,69 @@
+<?php
+
+function renderList()
+{
+	$key = 'nav';
+	$active = "home";
+	if(isset($_GET[$key]))
+	{
+		$active = $_GET[$key];
+	}
+	
+	renderMainList($active);
+	renderExtra($active);
+}
+
+function renderExtra($active)
+{
+	$extras = [
+		'service' => 'Serviço',
+		'agendamento' => 'Agendamento',
+		'services' => 'Serviços',
+	];
+
+	if(!array_key_exists($active, $extras))
+	{
+		return;
+	}
+	
+	$label = $extras[$active];
+
+	echo "<li class=extra>{$label}</li>";
+}
+
+function renderMainList($activeSection)
+{
+	$sections = [ 'home', 'agendamentos', 'tutores' ];
+	$sectionLabels = [ 'Home', 'Agendamentos', 'Tutores' ];
+
+	$active = array_search($activeSection, $sections);
+	if($active === false)
+	{
+		$active = -1;
+	}
+
+	for($i = 0; $i < count($sections); $i++)
+	{
+		renderMain($sections[$i], $sectionLabels[$i], $i == $active);
+	}
+}
+
+function renderMain($section, $label, bool $active)
+{
+	echo "<li><a href='index.php?nav={$section}'";
+
+	if($active)
+	{
+		echo " class='active'";
+	}
+
+	echo ">{$label}</a></li>";
+}
+
+?>
+
 <div class="navbar">
 	<ul>
-		<li><a href="index.php?nav=home" <?php if (!isset($_GET['nav']) || $_GET['nav'] === "home") echo "class='active'"; ?>>Home</a></li>
-		<li><a href="index.php?nav=agendamentos" <?php if (isset($_GET['nav']) && $_GET['nav'] === "agendamentos") echo "class='active'"; ?>>Agendamentos</a></li>
-		<li><a href="index.php?nav=tutores" <?php if (isset($_GET['nav']) && $_GET['nav'] === "tutores") echo "class='active'"; ?>>Tutores</a></li>
+		<?php renderList(); ?>
 	</ul>
 </div>
